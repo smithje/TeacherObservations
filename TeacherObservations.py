2c8a60e8fad9a9d49401cf90fcd13c48a31066ea
smithje <jeremy@musicsmith.net>
2013-02-28 19:42:30 -0500

    Update TeacherObservations.py

--- TeacherObservations.py
+++ TeacherObservations.py
@@ -163,8 +163,8 @@ Neither: %d
 First only: %d
 Second only: %d
 Total: %d
-Total agreement: %d""" % (cnt['both'], cnt['neither'], cnt['first'], cnt['second'], \
-                           sum(cnt.values()), cnt['both']+cnt['neither'])
+Total agreement: %d (%.2f%%)""" % (cnt['both'], cnt['neither'], cnt['first'], cnt['second'], \
+                           sum(cnt.values()), cnt['both']+cnt['neither'], (cnt['both']+cnt['neither'])/sum(cnt.values()))
     return cnt
     
 
